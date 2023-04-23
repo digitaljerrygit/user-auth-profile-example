b74388c3648d686f0d0ea437de99ad11d9748b06
digitaljerry <78813821+digitaljerrygit@users.noreply.github.com>
2023-04-23 06:09:52 -0400

    removed server proxies

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-      "/api-prod": {
-        target: "user-auth-profile-example.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/prod-api/, ""),
-      },
-    },
-  },
 });
